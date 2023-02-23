684fb707c9dcef19da0ac8776e8e994a254b9154
onepopina <anyarasp@gmail.com>
2023-02-23 12:01:09 +0100

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startButton.addEventListener('click', () => {
+  refs.stopButton.disabled = false;
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+    refs.startButton.disabled = true;
+  }, 1000);
+});
+
+refs.stopButton.addEventListener('click', () => {
+  refs.startButton.disabled = false;
+  clearInterval(timerId);
+  refs.stopButton.disabled = true;
+});
